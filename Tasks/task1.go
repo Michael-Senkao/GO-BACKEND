fb90dd619451ce47b53e328778135e730bf4d77e
Michael Senkao <senkaomichael@gmail.com>
2025-11-28 10:05:29 +0000

    Implement calcSum function to sum array elements
    Added a function to calculate the sum of an integer array.

--- Tasks/task1.go
+++ Tasks/task1.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func calcSum(arr []int) int{
+	sum := 0
+	for _, val := range arr{
+		sum += val
+	}
+
+	return sum
+}
 func main(){
 	fmt.Println("Hello, World!")
 }
